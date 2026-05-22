d8ca1003b45d693d41fbc76c3d3d538fa7159c83
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-05-22 00:19:58 -0300

    Atualizado

--- Apartaments.cpp
+++ Apartaments.cpp
@@ -16,9 +16,12 @@ int main() {
         cin >> aptSizeOferecido[j];
     }
 
+    sort(aptSizeDesejado.begin(), aptSizeDesejado.end());
+    sort(aptSizeOferecido.begin(), aptSizeOferecido.end());
+
     for (int k = 0; k < aptSizeDesejado.size(); k++) {
         for (int l = 0; l < aptSizeOferecido.size(); l++) {
-            if (abs(aptSizeDesejado[k] - aptSizeOferecido[l] <= difMaxima)) {
+            if (abs(aptSizeDesejado[k] - aptSizeOferecido[l]) <= difMaxima) {
                 contador += 1;
                 aptSizeOferecido.erase(aptSizeOferecido.begin() + l);
                 break;
