e8bc8fa3d6d8022f47829e831d3ea7aebcbabbc0
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-05-22 00:03:31 -0300

    Atualizado

---
+++ Apartaments.cpp
@@ -0,0 +1,30 @@
+#include "Apartaments.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int nCandidatos, nApartamentos, difMaxima, contador = 0;
+    cin >> nCandidatos >> nApartamentos >> difMaxima;
+    vector<int> aptSizeDesejado(nCandidatos);
+    vector<int> aptSizeOferecido(nApartamentos);
+
+    for (int i = 0; i < nCandidatos; i++) {
+        cin >> aptSizeDesejado[i];
+    }
+
+    for (int j = 0; j < nApartamentos; j++) {
+        cin >> aptSizeOferecido[j];
+    }
+
+    for (int k = 0; k < aptSizeDesejado.size(); k++) {
+        for (int l = 0; l < aptSizeOferecido.size(); l++) {
+            if (abs(aptSizeDesejado[k] - aptSizeOferecido[l] <= difMaxima)) {
+                contador += 1;
+                aptSizeOferecido.erase(aptSizeOferecido.begin() + l);
+                break;
+            }
+        }
+    }
+    cout << contador << endl;
+    return 0;
+}
